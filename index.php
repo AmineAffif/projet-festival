a0f6f37fc8e3f21d0cd3531b9dea838cdb6bcfea
Amine Affif <affif.amine@live.fr>
2020-11-13 22:07:08 +0100

    added the section2 and the footer

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="icon" href="./assets/fav_icon.ico" />
     <title>Projet Festival</title>
+    <!-- Add icon library -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <!--linking css-->
     <link rel="stylesheet" href="css/index.css">
     <!-- Bootstrap CSS -->
@@ -72,11 +74,80 @@
                     <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" class="shape-fill"></path>
                 </svg>
             </div>
+            <div class="custom-shape-divider-bottom-1605300780">
+                <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                    <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" class="shape-fill"></path>
+                    <path d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z" opacity=".5" class="shape-fill"></path>
+                    <path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" class="shape-fill"></path>
+                </svg>
+            </div>
+            <div class="sec2_wrapper">
+                <p>Parce que le festival représente un tremplin vers
+                    des scènes d’envergure nationale.</p>
+                <p>Inscrivez vous dès maintenant pour être un acteur
+                    de ce moment unique que vous vivrez
+                    avec vos plus grand fan.</p>
+                <div class="btn_signup_wrapper">
+                    <div class="left_stroke"></div>
+                    <button type="button" class="btn btn-dark">Inscription</button>
+                    <div class="right_stroke"></div>
+                </div>
+            </div>
         </div>
 
     </main>
 
-    <footer>
+
+    <footer class="page-footer font-small blue pt-4">
+
+        <!-- Footer Links -->
+        <div class="container-fluid text-center text-md-left">
+
+            <!-- Grid row -->
+            <div class="row">
+
+                <!-- Grid column -->
+                <div class="col-md-6 mt-md-0 mt-3 left_foot" >
+                    <div class="footer_sign">
+                        <img src="assets/images/fav_icon.jpg" alt="paradise-festival-logo">
+                        <h5 class="text-uppercase">Paradise Festival</h5>
+                    </div>
+
+                    <p class="citation">"La musique est la langue des émotions." S. Kant</p>
+
+                </div>
+
+
+                <hr class="clearfix w-100 d-md-none pb-3">
+
+
+                <div class="col-md-3 mb-md-0 mb-3">
+
+                    <!-- Links -->
+                    <h5 class="text-uppercase">Contact</h5>
+
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="https://www.instagram.com/creamine_/" class="fa fa-instagram"></a>
+                            <a href="https://www.instagram.com/creamine_/">Amine Affif</a>
+                        </li>
+                        <li>
+                            <a href="https://www.instagram.com/paradiise.beats/" class="fa fa-instagram"></a>
+                            <a href="https://www.instagram.com/paradiise.beats/">Theo Boudard</a>
+                        </li>
+                    </ul>
+
+                </div>
+
+
+            </div>
+
+        </div>
+
+        <!-- Copyright -->
+        <div class="footer-copyright text-center py-3">© 2020 Copyright:
+            <a href="https://mdbootstrap.com/"> paradise-festival.fr</a>
+        </div>
 
     </footer>
 </body>
