2f9386440ec45ed61ea307704970406573b4d95d
Amine Affif <affif.amine@live.fr>
2020-11-13 18:51:55 +0100

    added the landing page

--- index.php
+++ index.php
@@ -3,13 +3,37 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="icon" href="./assets/fav_icon.ico" />
     <title>Projet Festival</title>
+    <!--linking css-->
+    <link rel="stylesheet" href="css/index.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
 </head>
 <body>
-<h1>AMINE</h1>
+    <header>
+        <!--top navigation-->
+        <div class="nav_top">
+            <div class="empty_class-top"></div>
+            <div class="login_signup-wrapper">
+                <a href="#"><span>inscription</span></a>
+                <a href="#"><span class="right_span-top">connexion</span></a>
+            </div>
+        </div>
+        <!--main content in landing-->
+        <div class="main_landing">
+
+        </div>
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+
+    </footer>
 </body>
 </html>
 
